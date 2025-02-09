37e12be807e0ae8a4b71577be2b1e924480f0e2c
m-wkr <maylun.mw@outlook.com>
2025-02-09 13:48:02 -0500

    Added semantic errors to the parser

--- parser.cpp
+++ parser.cpp
@@ -1,4 +1,3 @@
-
 #include <stack>
 #include <deque>
 #include "parser.hpp"
@@ -13,47 +12,53 @@ nodeSharedPtr formAST(std::deque<token> &queue) {
 
 	tokenStack.push(copyRoot); 
 
-	while (!queue.empty()) {
+	try {
+    while (!queue.empty()) {
 
-		//Get the current token
-		token currentToken = queue.front();
-		queue.pop_front();
-
-
-		tokenType currentTokenType = currentToken.getTokenType();
-
-		nodeSharedPtr parentNode = tokenStack.top();
-
-		if (currentTokenType == S_TAG) {
+      //Get the current token
+      token currentToken = queue.front();
+      queue.pop_front();
 
 
-			nodeSharedPtr newNode = std::make_shared<node>(currentToken);
-			parentNode->addChild(newNode);
+      tokenType currentTokenType = currentToken.getTokenType();
 
-			tokenStack.push(newNode);
+      nodeSharedPtr parentNode = tokenStack.top();
 
-		} else if (currentTokenType == E_TAG) {
-			
-			if (parentNode->getTokenType() == S_TAG && parentNode->getTokenName() == currentToken.getTokenName()) {
+      if (currentTokenType == S_TAG) {
 
-				tokenStack.pop();
-			} else {
-				//raise error
-			}
+        nodeSharedPtr newNode = std::make_shared<node>(currentToken);
+        parentNode->addChild(newNode);
 
-		} else if (currentTokenType == SE_TAG) {
+        tokenStack.push(newNode);
 
-			nodeSharedPtr newNode = std::make_shared<node>(currentToken);
+      } else if (currentTokenType == E_TAG) {
+        
+        if (parentNode->getTokenType() == S_TAG && parentNode->getTokenName() == currentToken.getTokenName()) {
 
-			parentNode->addChild(newNode);
+          tokenStack.pop();
+        } else {
+          //raise error
+          throw ("Error: Unmatched closed tag used\n");
+        }
+
+      } else if (currentTokenType == SE_TAG) {
+
+        nodeSharedPtr newNode = std::make_shared<node>(currentToken);
+
+        parentNode->addChild(newNode);
 
 
-		} else {
-			//raise error
-		}
+      } else {
+        //raise error
+        throw ("Error: NIL tag utilised\n");
+      }
+    }
+
+    preorderTraversal(ROOT);
+
 	}
+	catch (const char* errorMsg) {std::cout << errorMsg;}
 
-	preorderTraversal(ROOT);
 	return tokenStack.top();
 
 }
